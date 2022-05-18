3cdf053a77eacec083fe421b4b0563251122858a
SafwenHali <93746781+SafwenHali@users.noreply.github.com>
2022-05-19 01:35:31 +0200

    first

---
+++ Database.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCqrzHcexkRraXbFDX5ThFyW3Kn_pvmIOs",
+  authDomain: "pet-hotelspa.firebaseapp.com",
+  projectId: "pet-hotelspa",
+  storageBucket: "pet-hotelspa.appspot.com",
+  messagingSenderId: "244454942843",
+  appId: "1:244454942843:web:c6367a61cf579b4f0c9ade"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
